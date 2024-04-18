a3fbfbbaca6a307d3077a8ffea481775640f5c40
Domnul Elias <eliastarin19@yahoo.com>
2024-04-18 13:38:42 +0200

    Init
    Init

---
+++ src/test/java/nl/inholland/BankAPI/BankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.inholland.BankAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
