16ca00acc61737958edef1db406108a4d9e1bd9b
Andy Rodriguez <577147@student.inholland.nl>
2024-05-07 13:19:17 +0200

    Revert "Merge branch 'master' of https://github.com/eliastarin/BankAPI"
    This reverts commit c7e0a7df253d0b10f02a48955e7fc4e31d12d70d, reversing
    changes made to 48122bfe70f4701e796d8109dfa531d4cea2a770.

--- Bank_rest_api/src/main/java/nl/inholland/BankAPI/Model/Account.java
+++ Bank_rest_api/src/main/java/nl/inholland/BankAPI/Model/Account.java
@@ -1,11 +1,4 @@
 package nl.inholland.BankAPI.Model;
 
 public class Account {
-
-    private String iban;
-    private double balance;
-    private double dailyLimit;
-    private double absoluteLimit;
-    private AccountType accountType;
-    //private list transactionHistory;
 }
--- Bank_rest_api/src/main/java/nl/inholland/BankAPI/Model/User.java
+++ Bank_rest_api/src/main/java/nl/inholland/BankAPI/Model/User.java
@@ -2,13 +2,4 @@ package nl.inholland.BankAPI.Model;
 
 public class User {
 
-    private String firstName;
-    private String lastName;
-    private long bsn;
-    private String email;
-    private UserType userType;
-    private long phoneNumber;
-
-
-
 }
