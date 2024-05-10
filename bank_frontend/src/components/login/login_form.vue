81454577cf4c6d6fdc02ce0557c21c21b5382a3c
Andy Rodriguez <577147@student.inholland.nl>
2024-05-10 16:39:43 +0200

    adding the front end

---
+++ bank_frontend/src/components/login/login_form.vue
@@ -0,0 +1,6 @@
+<template>
+<h1>this is the login page</h1>
+</template>
+<script>
+
+</script>
---
+++ bank_frontend/src/views/LoginView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import LoginForm from '../components/login/login_form.vue'
+</script>
+<template>
+    <LoginForm />
+</template>
+<style>
+</style>
