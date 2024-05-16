f91d08b0ec4cd0b8bb50f133d57847e6b5041a43
Sara-Eftekhar-Azam <90862126+Sara-Eftekhar-Azam@users.noreply.github.com>
2024-05-16 17:42:09 +0200

    Update TransactionRepository.java

--- Bank_rest_api/src/main/java/nl/inholland/BankAPI/Repository/TransactionRepository.java
+++ Bank_rest_api/src/main/java/nl/inholland/BankAPI/Repository/TransactionRepository.java
@@ -1,4 +1,10 @@
 package nl.inholland.BankAPI.Repository;
 
-public class TransactionRepository {
+import nl.inholland.BankAPI.Model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
 }
