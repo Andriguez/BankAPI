d6e342a3f7ff20ee4ee9a247c261d47623a9ed05
Sara-Eftekhar-Azam <90862126+Sara-Eftekhar-Azam@users.noreply.github.com>
2024-05-16 17:41:58 +0200

    Update AccountRepository.java

--- Bank_rest_api/src/main/java/nl/inholland/BankAPI/Repository/AccountRepository.java
+++ Bank_rest_api/src/main/java/nl/inholland/BankAPI/Repository/AccountRepository.java
@@ -4,10 +4,8 @@ import nl.inholland.BankAPI.Model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    List<Account> findAccountsByUsername(String username);
 }
