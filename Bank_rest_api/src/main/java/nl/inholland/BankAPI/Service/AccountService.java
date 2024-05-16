7d6cee49c374c0a3a654179c53ceb9917b29d3f5
Sara-Eftekhar-Azam <90862126+Sara-Eftekhar-Azam@users.noreply.github.com>
2024-05-16 17:42:20 +0200

    Update AccountService.java

--- Bank_rest_api/src/main/java/nl/inholland/BankAPI/Service/AccountService.java
+++ Bank_rest_api/src/main/java/nl/inholland/BankAPI/Service/AccountService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import javax.naming.AuthenticationException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AccountService {
@@ -15,17 +16,40 @@ public class AccountService {
         this.accountRepository = accountRepository;
     }
 
-    public List<Account> getAccounts(String username) {
-        List<Account> accounts;
+    public List<Account> getAccounts(String username, String iban) {
+        List<Account> accounts = accountRepository.findAll();
         if(username != null && !username.isEmpty()) {
-            accounts = accountRepository.findAccountsByUsername(username);
+            accounts = accounts.stream()
+                    .filter(account -> username.equals(account.getUsername()))
+                    .collect(Collectors.toList());
             System.out.println("found by username " + username );
             System.out.println(accounts.size());
         }
-        else {
-            accounts = accountRepository.findAll();
-            System.out.println("found all" );
+        if(iban != null && !iban.isEmpty()) {
+            accounts = accounts.stream()
+                    .filter(account -> iban.equals(account.getIban()))
+                    .collect(Collectors.toList());
         }
         return accounts;
+    }
+
+    public boolean isUserAccount(String j_username, String toFromIban) {
+        List<Account> accounts = getAccounts(j_username, toFromIban);
+        if (accounts.size() == 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public Account getAccountByIban(String toFromIban) {
+        List<Account> accounts = getAccounts(null, toFromIban);
+        if(accounts.size() == 1) {
+            return accounts.get(0);
+        }
+        else {
+            return null;
+        }
     }
 }
