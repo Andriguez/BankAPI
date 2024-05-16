0f1a3ac1956516b8ac6486d027844b0fc289763c
Sara-Eftekhar-Azam <90862126+Sara-Eftekhar-Azam@users.noreply.github.com>
2024-05-16 17:41:50 +0200

    Update Account.java

--- Bank_rest_api/src/main/java/nl/inholland/BankAPI/Model/Account.java
+++ Bank_rest_api/src/main/java/nl/inholland/BankAPI/Model/Account.java
@@ -26,6 +26,11 @@ public class Account {
     private User owner;
 
     private String username;
+    public long getId() {return id;}
+    public String getUsername() {return username;}
+    public void setUsername(String username){
+        this.username = username;
+    }
 
     public String getIban(){
         return iban;
