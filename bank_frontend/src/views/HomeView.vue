1f5568aa9ffe8213d740db5a6bb108efd10a50ee
Andy Rodriguez <577147@student.inholland.nl>
2024-05-17 19:41:06 +0200

    minor tweaks

--- bank_frontend/src/views/HomeView.vue
+++ bank_frontend/src/views/HomeView.vue
@@ -3,9 +3,9 @@
 
 <template>
   <div class="container p-5" style="text-align: center;">
-    <h1 class="py-4">Welcome to Holla Dolla Bank</h1>
-    <h3 class="py-2">Your Trusted Financial Partner</h3>
-    <h5>At Holla Dolla Bank, we believe in empowering your financial journey with innovative solutions and personalized services. Whether you're looking to open a new account, secure a loan, or explore investment opportunities, our dedicated team is here to assist you every step of the way.</h5>
+    <h1 class="py-4">Welcome to <em>Holla Dolla Bank</em></h1>
+    <h3 class="py-2" style="color: white;">Your Trusted Financial Partner</h3>
+    <h5>At <em>Holla Dolla Bank</em>, we believe in empowering your financial journey with innovative solutions and personalized services. Whether you're looking to open a new account, secure a loan, or explore investment opportunities, our dedicated team is here to assist you every step of the way.</h5>
   </div>
 </template>
 
