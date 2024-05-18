2a99e397085367ceee1a7d343273276208ae0116
Andy Rodriguez <577147@student.inholland.nl>
2024-05-18 21:22:29 +0200

    fixed little problem with username/email

--- Bank_rest_api/src/main/java/nl/inholland/BankAPI/Model/DTO/UserDTO.java
+++ Bank_rest_api/src/main/java/nl/inholland/BankAPI/Model/DTO/UserDTO.java
@@ -5,11 +5,11 @@ import nl.inholland.BankAPI.Model.UserType;
 
 import java.util.List;
 
-public record UserDTO(String firstName, String lastName, String username, String password, long phoneNumber, long bsnNumber, List<UserType> userType) {
+public record UserDTO(String firstName, String lastName, String email, String password, long phoneNumber, long bsnNumber, List<UserType> userType) {
     public UserDTO(User user){
         this(user.getFirstName(),
                 user.getLastName(),
-                user.getUsername(),
+                user.getEmail(),
                 user.getPassword(),
                 user.getPhoneNumber(),
                 user.getBsnNumber(),
