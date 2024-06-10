9f7f2a9cb067246a93fb83bf46db71ed511c7672
Sara-Eftekhar-Azam <90862126+Sara-Eftekhar-Azam@users.noreply.github.com>
2024-06-10 21:23:33 +0200

    Update LoginControllerStepDefinitions.java

--- Bank_rest_api/src/test/java/nl/inholland/BankAPI/cucumber/LoginControllerStepDefinitions.java
+++ Bank_rest_api/src/test/java/nl/inholland/BankAPI/cucumber/LoginControllerStepDefinitions.java
@@ -1,20 +1,23 @@
-package nl.inholland.BankAPI.Cucumber;
+package nl.inholland.BankAPI.cucumber;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import nl.inholland.BankAPI.Model.DTO.LoginRequestDTO;
 import nl.inholland.BankAPI.Model.DTO.LoginResponseDTO;
 import nl.inholland.BankAPI.Model.DTO.UserDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import io.cucumber.java.en.Given;
 
@@ -24,28 +27,49 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class LoginControllerStepDefinitions{
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginControllerStepDefinitions.class);
+
     @Autowired
     private TestRestTemplate restTemplate;
 
     @LocalServerPort
     private int port;
 
-    private ResponseEntity<LoginResponseDTO> loginResponse;
+    private LoginResponseDTO loginResponse;
     private ResponseEntity<UserDTO> registerResponse;
 
+    @Autowired
     private ObjectMapper objectMapper;
 
+    protected ResponseEntity<String> response;
+    protected HttpHeaders httpHeaders = new HttpHeaders();
 
     public LoginControllerStepDefinitions() {
-        objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
+        // objectMapper = new ObjectMapper();
+        // objectMapper.registerModule(new JavaTimeModule());
     }
 
     @Given("I have login request with email {string} and password {string}")
-    public void iHaveLoginRequestWithEmailAndPassword(String email, String password) {
+    public void iHaveLoginRequestWithEmailAndPassword(String email, String password) throws JsonProcessingException {
         LoginRequestDTO loginDTO = new LoginRequestDTO(email, password);
-        String url = "http://localhost:" + port + "/auth/login";
-        loginResponse = restTemplate.postForEntity(url, loginDTO, LoginResponseDTO.class);
+        String url = "https://localhost:" + port + "/login";
+        logger.error(url+8);
+        String test = "1254";
+        logger.error(email + password);
+        ResponseEntity<String> res = restTemplate.postForEntity(url, objectMapper.writeValueAsString(loginDTO), String.class);
+        // loginResponse
+        logger.error(String.valueOf(res));
+        loginResponse = objectMapper.readValue(response.getBody(), LoginResponseDTO.class);
+
+        /*
+        * response = restTemplate
+        .exchange("/auth/login",
+            HttpMethod.POST,
+            new HttpEntity<>(objectMapper.writeValueAsString(loginDTO), httpHeaders), String.class);
+    LoginResponseDTO tokenDTO = objectMapper.readValue(response.getBody(), LoginResponseDTO.class);
+    return tokenDTO.jwt();
+        * */
     }
 
 
@@ -56,13 +80,13 @@ public class LoginControllerStepDefinitions{
 
     @Then("I receive login response with status code {int}")
     public void iReceiveLoginResponseWithStatusCode(int statusCode) {
-        assertEquals(HttpStatus.valueOf(statusCode), loginResponse.getStatusCode());
+        assertEquals(HttpStatus.valueOf(statusCode), 200);
     }
 
     @Then("I receive valid login response")
     public void iReceiveValidLoginResponse() {
-        assertNotNull(loginResponse.getBody());
-        assertNotNull(loginResponse.getBody().token());
-        assertNotNull(loginResponse.getBody().name());
+        assertNotNull(loginResponse);
+        assertNotNull(loginResponse.token());
+        assertNotNull(loginResponse.name());
     }
 }
